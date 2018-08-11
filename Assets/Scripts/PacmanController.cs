f92ec0a12d7ae13fbedb7e0c9e3c17cbf4e6266e
Arthur Uzulin <fuj1nman@hotmail.com>
2018-08-11 15:30:50 +1000

    Basic Pacman Resources and broken movement

---
+++ Assets/Scripts/PacmanController.cs
@@ -0,0 +1,130 @@
+﻿using UnityEngine;
+
+public class PacmanController : MonoBehaviour {
+    public Direction direction = Direction.NONE;
+
+    public SpriteAnimator aliveAnimator;
+    public SpriteAnimator deadAnimator;
+
+    public float speed = 1.5F;
+
+    private Direction directionBuffer = Direction.NONE;
+
+    private void Awake()
+    {
+        if(!aliveAnimator)
+        {
+            Debug.LogError("No alive animator set");
+            Destroy(this);
+        }
+    }
+
+    private void Update()
+    {
+        if (true && direction != Direction.NONE) // TODO: if alive
+            aliveAnimator.playing = true;
+
+        if (Input.GetKey(KeyCode.UpArrow))
+            directionBuffer = Direction.UP;
+        if (Input.GetKey(KeyCode.DownArrow))
+            directionBuffer = Direction.DOWN;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            directionBuffer = Direction.LEFT;
+        if (Input.GetKey(KeyCode.RightArrow))
+            directionBuffer = Direction.RIGHT;
+
+        if (CanTurn()) {
+            direction = directionBuffer;
+        }
+
+        if (direction == Direction.NONE)
+            return;
+
+        transform.eulerAngles = new Vector3(0, 0, (int)direction);
+
+        if (CanMove())
+            transform.Translate(Vector2.right * Time.deltaTime * speed);
+    }
+
+    private bool CanTurn()
+    {
+        if (directionBuffer == Direction.NONE)
+            return false;
+
+        Ray2D ray = CreateRay(doOriginShift: CanMove());
+        Debug.DrawRay(ray.origin, ray.direction, Color.magenta);
+        return !Physics2D.Raycast(ray.origin, ray.direction, ray.origin == (Vector2)transform.position ? .26F : .51F);
+    }
+
+    private bool CanMove()
+    {
+        if (direction == Direction.NONE)
+            return false;
+
+        Ray2D ray = CreateRay(direction);
+        Debug.DrawRay(ray.origin, ray.direction, Color.cyan);
+        return !Physics2D.Raycast(ray.origin, ray.direction, ray.origin == (Vector2)transform.position ? .26F : .51F);
+    }
+
+    private Ray2D CreateRay(Direction dirOverride = Direction.NONE, bool doOriginShift = true)
+    {
+        Direction dirTo = dirOverride == Direction.NONE ? directionBuffer : dirOverride;
+
+        if (dirTo == Direction.NONE)
+            return new Ray2D();
+
+        Vector2 origin = Vector2.zero;
+        Vector2 vecDir = Vector2.zero;
+
+        if (doOriginShift)
+            switch (direction)
+            {
+                case Direction.UP:
+                    origin = (Vector2)transform.position + new Vector2(0F, -.24F);
+                    break;
+                case Direction.DOWN:
+                    origin = (Vector2)transform.position + new Vector2(0F, .24F);
+                    break;
+                case Direction.LEFT:
+                    origin = (Vector2)transform.position + new Vector2(.24F, 0F);
+                    break;
+                case Direction.RIGHT:
+                    origin = (Vector2)transform.position + new Vector2(-.24F, 0F);
+                    break;
+                default:
+                    origin = transform.position;
+                    break;
+            }
+        else
+            origin = transform.position;
+
+        switch (dirTo)
+        {
+            case Direction.UP:
+                vecDir = Vector2.up;
+                break;
+            case Direction.DOWN:
+                vecDir = Vector2.down;
+                break;
+            case Direction.LEFT:
+                vecDir = Vector2.left;
+                break;
+            case Direction.RIGHT:
+                vecDir = Vector2.right;
+                break;
+            default:
+                return new Ray2D();
+        }
+
+        return new Ray2D(origin, vecDir);
+    }
+
+    public enum Direction
+    {
+        UP = 90,
+        DOWN = -90,
+        LEFT = -180,
+        RIGHT = 0,
+        NONE = -1337
+    }
+}
---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,86 @@
+﻿using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour {
+    public Sprite[] frames;
+    public int frame;
+
+    public bool playing = true;
+
+    public float frameRate = 60;
+
+    public AnimationRestartMode mode;
+
+    private bool isLoopingBack = false;
+    private float frameTime = 0F;
+
+    private SpriteRenderer render;
+
+    private void Start()
+    {
+        render = GetComponent<SpriteRenderer>();
+
+        Reload();
+    }
+
+    public void Update()
+    {
+        if (!playing)
+            return;
+
+        float spf = 1 / frameRate;
+
+        frameTime += Time.deltaTime;
+
+        if (frameTime > spf)
+        {
+            frameTime = 0;
+            AdvanceAnimation();
+        }
+    }
+
+    private void AdvanceAnimation()
+    {
+        if (mode == AnimationRestartMode.LOOPBACK)
+        {
+            if (isLoopingBack)
+            {
+                frame--;
+                if(frame == 0)
+                {
+                    isLoopingBack = false;
+                }
+            }
+            else
+            {
+                frame++;
+                if (frame >= frames.Length)
+                {
+                    isLoopingBack = true;
+                    frame--;
+                }
+            }
+        }
+        else
+        {
+            frame++;
+            if (frame >= frames.Length)
+                if (mode == AnimationRestartMode.STOP)
+                    playing = false;
+                else
+                    frame = 0;
+        }
+        Reload();
+    }
+
+    private void Reload()
+    {
+        render.sprite = frames[frame];
+    }
+
+    public enum AnimationRestartMode
+    {
+        RESET,
+        LOOPBACK,
+        STOP
+    }
+}
